119d410404323485fb6265a78da3640513699e35
Petrik van der Velde <petrikvandervelde@gmail.com>
2016-05-02 22:19:08 +1200

    Fixing incorrect parameters
    references #2

--- scripts/New-DiscoveryHypervImage.ps1
+++ scripts/New-DiscoveryHypervImage.ps1
@@ -27,93 +27,27 @@
     .PARAMETER hypervHostVmStoragePath
 
     The UNC path to the directory that stores the Hyper-V VM information.
-
-
-    .PARAMETER dataCenterName
-
-    The name of the consul data center to which the remote machine should belong once configuration is completed.
-
-
-    .PARAMETER clusterEntryPointAddress
-
-    The DNS name of a machine that is part of the consul cluster to which the remote machine should be joined.
-
-
-    .PARAMETER globalDnsServerAddress
-
-    The DNS name or IP address of the DNS server that will be used by Consul to handle DNS fallback.
-
-
-    .PARAMETER environmentName
-
-    The name of the environment to which the remote machine should be added.
-
-
-    .PARAMETER consulLocalAddress
-
-    The URL to the local consul agent.
-
-
-    .EXAMPLE
-
-    New-WindowsResource -computerName "MyCoolServer"
 #>
 [CmdletBinding()]
 param(
     [Parameter(Mandatory = $true)]
     [string] $osName                                            = '',
 
-    [Parameter(Mandatory = $true,
-               ParameterSetName = 'FromUserSpecification')]
+    [Parameter(Mandatory = $true)]
     [string] $hypervHost                                        = '',
 
-    [Parameter(Mandatory = $true,
-               ParameterSetName = 'FromUserSpecification')]
+    [Parameter()]
     [string] $vhdxTemplatePath                                  = "\\$($hypervHost)\vmtemplates",
 
-    [Parameter(Mandatory = $true,
-               ParameterSetName = 'FromUserSpecification')]
-    [string] $hypervHostVmStoragePath                           = "\\$(hypervHost)\vms\machines",
-
-    [Parameter(Mandatory = $true,
-               ParameterSetName = 'FromUserSpecification')]
-    [string] $dataCenterName                                    = '',
-
-    [Parameter(Mandatory = $true,
-               ParameterSetName = 'FromUserSpecification')]
-    [string] $clusterEntryPointAddress                          = '',
-
-    [Parameter(Mandatory = $false,
-               ParameterSetName = 'FromUserSpecification')]
-    [string] $globalDnsServerAddress                            = '',
-
-    [Parameter(Mandatory = $true,
-               ParameterSetName = 'FromMetaCluster')]
-    [string] $environmentName                                   = 'Development',
-
-    [Parameter(Mandatory = $false,
-               ParameterSetName = 'FromMetaCluster')]
-    [string] $consulLocalAddress                                = "http://localhost:8500"
+    [Parameter()]
+    [string] $hypervHostVmStoragePath                           = "\\$($hypervHost)\vms\machines"
 )
 
 Write-Verbose "New-DiscoveryHypervImage - osName = $osName"
 Write-Verbose "New-DiscoveryHypervImage - hypervHost: $hypervHost"
-switch ($psCmdlet.ParameterSetName)
-{
-    'FromUserSpecification' {
-        Write-Verbose "New-DiscoveryHypervImage - hypervHost = $hypervHost"
-        Write-Verbose "New-DiscoveryHypervImage - vhdxTemplatePath = $vhdxTemplatePath"
-        Write-Verbose "New-DiscoveryHypervImage - hypervHostVmStoragePath = $hypervHostVmStoragePath"
-        Write-Verbose "New-DiscoveryHypervImage - dataCenterName = $dataCenterName"
-        Write-Verbose "New-DiscoveryHypervImage - clusterEntryPointAddress = $clusterEntryPointAddress"
-        Write-Verbose "New-DiscoveryHypervImage - globalDnsServerAddress = $globalDnsServerAddress"
-    }
+Write-Verbose "New-DiscoveryHypervImage - vhdxTemplatePath = $vhdxTemplatePath"
+Write-Verbose "New-DiscoveryHypervImage - hypervHostVmStoragePath = $hypervHostVmStoragePath"
 
-    'FromMetaCluster' {
-        Write-Verbose "New-DiscoveryHypervImage - environmentName = $environmentName"
-        Write-Verbose "New-DiscoveryHypervImage - consulLocalAddress = $consulLocalAddress"
-    }
-}
 
 # Stop everything if there are errors
 $ErrorActionPreference = 'Stop'
@@ -128,29 +62,12 @@ $commonParameterSwitches =
 try
 {
     $installationScript = Join-Path $PSScriptRoot 'Initialize-HyperVImage.ps1'
-    switch ($psCmdlet.ParameterSetName)
-    {
-        'FromUserSpecification' {
-            & $installationScript `
-                -osName $osName `
-                -hypervHost $hypervHost `
-                -vhdxTemplatePath $vhdxTemplatePath `
-                -hypervHostVmStoragePath $hypervHostVmStoragePath `
-                -dataCenterName $dataCenterName `
-                -clusterEntryPointAddress $clusterEntryPointAddress `
-                -globalDnsServerAddress $globalDnsServerAddress `
-                @commonParameterSwitches
-        }
-
-        'FromMetaCluster' {
-            & $installationScript `
-                -osName $osName `
-                -hypervHost $hypervHost `
-                -environmentName $environmentName `
-                -consulLocalAddress $consulLocalAddress `
-                @commonParameterSwitches
-        }
-    }
+    & $installationScript `
+        -osName $osName `
+        -hypervHost $hypervHost `
+        -vhdxTemplatePath $vhdxTemplatePath `
+        -hypervHostVmStoragePath $hypervHostVmStoragePath `
+        @commonParameterSwitches
 }
 catch
 {
